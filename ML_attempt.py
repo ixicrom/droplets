07c9165ff65e7f64c13de11e8bf5fde8b35db772
Emily Gould <e.m.gould@sms.ed.ac.uk>
2020-03-30 17:53:20 +0100

    Worked on ML_attempt, now it will work on a single variable
    It can't be as smart as I want it, I will have to do further data processing in order to cluster my images - it can't work on the raw data because it needs a 2D input not 3D.

--- ML_attempt.py
+++ ML_attempt.py
@@ -1,7 +1,9 @@
 from slice_tools import slice_image
-import os
+# import os
+import glob
 import pandas as pd
 from sklearn import cluster
+import numpy as np
 
 def make_files():
     filePath='/Users/s1101153/Dropbox/Emily/'
@@ -22,12 +24,14 @@ def make_files():
             im_slices=slice_image(fileName, umSize, pxSize, Cx, Cy, n_slice, True)
 
 def read_files(folderName):
-    file_names=[folderName+x for x in os.listdir(folderName)]
+    search=os.path.join(folderName, "*.pkl")
+    file_names=glob.glob(search)#folderName+x for x in os.listdir(folderName)]
     dat=[]
     keys=[]
     for file in file_names:
-        entry=pd.read_pickle(file)
+        entry=pd.read_pickle(file) #actually reading the file
         dat.append(entry)
+        # making nice column names_______
         start1=file.find("T")
         end1=file.find("_63xoil")
         part1=file[start1:end1]
@@ -38,30 +42,30 @@ def read_files(folderName):
         part3=file[start3:]
         key=part1+part2+part3
         keys.append(str(key))
-    print([x for x in keys if keys.count(x) >= 2])
+        # ________________________________
+    # print([x for x in keys if keys.count(x) >= 2])
     dat_df=pd.concat(dat, axis=1, keys=keys) #axis=1 for side-by-side. will do multi-layer column names
     return dat_df
 
 def main():
     # make_files()
-    dat=read_files("/Users/s1101153/Dropbox/Emily/slices/")
-    print(dat)
+    dat=read_files("/Users/s1101153/Dropbox/Emily/")
     dat = dat.dropna()
+    dat=dat.transpose()
 
-    kmeans=cluster.KMeans().fit(dat)
-    print(kmeans)
+    dat=dat.to_numpy()
+    dat.shape
+
+    # type(dat)
+    # type(dat[0])
+    big_array=np.full((108,4,24400), np.nan)
+    for i in range(108):
+        j=i*4
+        little_array=dat[j:j+4, :]
+        big_array[i,]=little_array
+
+    kmeans=cluster.KMeans(n_clusters=8).fit(big_array[:,3,:])
+    labs=kmeans.labels_
+    len(labs)
+    np.savetxt('test.txt',np.asarray(labs))
 main()
-# dat=pd.read_pickle('/Users/s1101153/Dropbox/Emily/slices/SUM_2014_5_30-T3M_7_63xoil_1.tif_slice0')
-# print(dat)
-# folderName="/Users/s1101153/Dropbox/Emily/slices/"
-# test_file='SUM_2014_5_30-T3M_7_63xoil_1.tif_slice0'
-# start1=test_file.find("T")
-# end1=test_file.find("_63xoil")
-# part1=test_file[start1:end1]
-# start2=end1+7
-# end2=start2+2
-# part2=test_file[start2:end2]
-# start3=test_file.find(".tif")+4
-# part3=test_file[start3:]
-# test_file=part1+part2+part3
-# test_file
