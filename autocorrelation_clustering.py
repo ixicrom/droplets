43e4ee99a614303a51b82d96a70bc00a231fcb1e
Emily Gould <e.m.gould@sms.ed.ac.uk>
2020-05-15 17:30:25 +0100

    Neatened up autocorrelation_clustering.py

--- autocorrelation_clustering.py
+++ autocorrelation_clustering.py
@@ -1,76 +1,41 @@
 from slice_tools import *
-import os
+# import os
 import numpy as np
 import matplotlib.pyplot as pl
-import scipy.fftpack as fftim
-# from scipy.misc.pilutil import Image
+# import scipy.fftpack as fftim
 import pandas as pd
-from skimage import io
-import math
+# from skimage import io
+# import math
 from scipy.signal import correlate2d
 from sklearn import cluster, preprocessing
 from datetime import datetime
 
+def r_average(data):
+    y, x = np.indices((data.shape))
+    tbin = np.bincount(x.ravel(),data.ravel())
+    nx = np.bincount(x.ravel())
+    rav = tbin/nx
+    rav = rav[:int(len(x)/2)]
+    return rav
 
-# def r_average(data):
-#     y, x = np.indices((data.shape))
-#     tbin = np.bincount(x.ravel(),data.ravel())
-#     nx = np.bincount(x.ravel())
-#     rav = tbin/nx
-#     return rav
-
-# def radial_profile(data,centre):
-#     y, x = np.indices((data.shape))
-#     r = np.sqrt((x-centre[0])**2+(y-centre[1])**2)
-#     r = r.astype(np.int)
-#     tbin = np.bincount(r.ravel(),data.ravel())
-#     nr = np.bincount(r.ravel())
-#     radialprofile = tbin/nr
-#     return radialprofile
-
-# def correlate(x,y):
-#     fr = fftim.fft2(x)
-#     fr2 = fftim.fft2(np.flipud(np.fliplr(y)))
-#     m,n = fr.shape
-#     cc = np.real(fftim.ifft2(fr*fr2))
-#     cc = np.roll(cc, int(-m/2+1),axis=0)
-#     cc = np.roll(cc, int(-n/2+1),axis=1)
-#     return cc
 
 def main():
     filePath='/Users/s1101153/Dropbox/Emily/'
     dat=read_files(filePath)
     dat = dat.dropna()
 
-    dat
-
-
     idx=pd.IndexSlice
 
 # rescale the data______________________________________________
     x = dat.loc[:,idx[:,['val_green','val_red']]]
-    x.shape
     min_max_scaler = preprocessing.MinMaxScaler()
     x_scaled = min_max_scaler.fit_transform(x)
-    pd.DataFrame(x_scaled).describe()
     dat.loc[:,idx[:,['val_green','val_red']]] = x_scaled
-    dat
 # ____________________________________________________________
 
-    # r_vals = dat.loc[:,idx[:,'r']]
-    #
-    # theta_vals = dat.loc[:,idx[:,'theta']]
-    # theta_vals
-    #
-    # imFile = filePath+'SUM_2014_5_30-T3M_7_63xoil_1.tif'
-    # slice = slice_image(imFile, 72.58, 2048, 30.195, 34.342, 12, saveFile=False)
-
     sliceNames = dat.columns
-    sliceNames
     n_slice = int(len(sliceNames)/4)
-    n_slice
     ACF = pd.DataFrame()
-    # i=0
     for i in range(n_slice):
         index_g = sliceNames[i*4+2]
         index_r = sliceNames[i*4+3]
@@ -82,7 +47,6 @@ def main():
         im_r = np.zeros((num_r,100))
         r, t = 0, 0
 
-        slice_df['val_green'][0]
         for j in range(len(slice_df)):
             im_g[r,t]=slice_df['val_green'][j]
             im_r[r,t]=slice_df['val_red'][j]
@@ -93,19 +57,16 @@ def main():
                 t = t+1
         # pl.imshow(im_r)
         ac3_r = correlate2d(im_r,im_r)
+        # pl.imshow(ac3_r)
         # pl.imshow(im_g)
-        # autoCorr_r = r_average(ac3_r)
-        ac3_r.shape
         ac3_g = correlate2d(im_g,im_g)
-        # autoCorr_g = r_average(ac3_r)
+        # pl.imshow(ac3_g)
+        # r_average(ac3_g)[:100]
 
-        # print(col_r)
-        ACF[index_r]=ac3_r.ravel()
+        ACF[index_r]=r_average(ac3_r)[:100]
 
-        # print(col_g)
-        ACF[index_g]=ac3_g.ravel()
-        if i==1:
-            print(ACF)
+        ACF[index_g]=r_average(ac3_g)[:100]
+
 
     ACF = ACF.transpose()
     ACF.index = pd.MultiIndex.from_tuples(ACF.index)
@@ -124,18 +85,18 @@ def main():
     date = datetime.today().strftime('%Y-%m-%d')
     filePath = '/Users/s1101153/Dropbox/Emily/Results/'
 
-    # fileName = date + 'ACF-clusters.csv'
-    # outFile = filePath+fileName
-    # dat_toPlot.to_csv(outFile, header=True)
-    # print('File saved: '+outFile)
+    fileName = date + '_ACF_clusters_1d-av.csv'
+    outFile = filePath+fileName
+    dat_toPlot.to_csv(outFile, header=True)
+    print('File saved: '+outFile)
 
 
-    fileShort = date + '_ACF_clusters_only.csv'
-    out2 = filePath + fileShort
-    dat_clusters = dat_toPlot.drop(['ACF_index','Value'], axis='columns').drop_duplicates()
-    dat_clusters = dat_clusters.reset_index(drop=True)
-    dat_clusters.to_csv(out2, header=True)
-    print('File saved: '+out2)
+    # fileName = date + '_ACF_clusters_1d-av.csv'
+    # out2 = filePath + fileName
+    # # dat_clusters = dat_toPlot.drop(['ACF_index','Value'], axis='columns').drop_duplicates()
+    # # dat_clusters = dat_clusters.reset_index(drop=True)
+    # # dat_clusters.to_csv(out2, header=True)
+    # print('File saved: '+out2)
 
 
 main()
