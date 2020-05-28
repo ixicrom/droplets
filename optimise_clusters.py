5e1b4c84a09508d8b9c4f958b8b0228f5e0749b8
Emily Gould <e.m.gould@sms.ed.ac.uk>
2020-05-28 18:25:02 +0100

    Added gini and entropy measures to optimise_clusters.py

--- optimise_clusters.py
+++ optimise_clusters.py
@@ -2,6 +2,44 @@ from data_tools import *
 from slice_tools import *
 from sklearn import cluster
 import random
+import pandas as pd
+import matplotlib.pyplot as pl
+import math
+
+def measure_clusters(cluster_freq):
+    idx = pd.IndexSlice
+
+    sample_scores = list()
+    for col in cluster_freq.columns:
+        counts = cluster_freq[col].values
+        sumsq = np.sum(counts**2)
+        n_clust = np.count_nonzero(counts)
+        score = sumsq/n_clust
+        sample_scores.append(score)
+    return sample_scores
+
+def gini_index(cluster_freq):
+    sample_scores = list()
+    n_clust = cluster_freq.shape[0]
+    for col in cluster_freq.columns:
+        counts = cluster_freq[col].values
+        p = counts[counts!=0]/12
+        q = 1-p
+        G = sum(p*q)
+        sample_scores.append(G/n_clust**(1/3))
+    return sample_scores
+
+def entropy(cluster_freq):
+    sample_scores = list()
+    n_clust = cluster_freq.shape[0]
+    for col in cluster_freq.columns:
+        counts = cluster_freq[col].values
+        p = counts[counts!=0]/12
+        q = np.log(p)
+        D = -1*sum(p*q)
+        sample_scores.append(D/n_clust**(1/3))
+    return sample_scores
+
 
 def main():
     filePath='/Users/s1101153/Dropbox/Emily/'
@@ -13,8 +51,10 @@ def main():
     dat_r_bins = dat_all.groupby(np.arange(len(dat_all))//N).mean()
     dat_forLearning = dat_r_bins.transpose()
 
-    for n_clust in range(2,20):
-        n_clust = 8
+    cluster_scores_gini = list()
+    cluster_scores_ent = list()
+    cluster_scores = list()
+    for n_clust in range(2,100):
         np.random.seed(1234)
         kmeans=cluster.KMeans(n_clusters=n_clust).fit(dat_forLearning)
         labs=kmeans.labels_
@@ -27,26 +67,42 @@ def main():
         dat_toPlot.columns = ['Slice', 'Colour', 'Cluster_kmeans', 'r', 'Value']
         dat_toPlot['Sample'] = dat_toPlot['Slice'].str.slice(0,8)
         dat_toPlot = dat_toPlot.drop(['r', 'Value'], axis='columns').drop_duplicates().reset_index(drop=True)
-        cl_count = count_clusters(dat_toPlot, 'Sample', 'Colour', 'Cluster_kmeans')
-        cl_count.transpose().plot(kind='bar', stacked = True, title=str(n_clust), legend=False)
+        cl_count = count_clusters(dat_toPlot, grouper1='Sample', grouper2='Colour', counter='Cluster_kmeans')
+        # cl_count.transpose().plot(kind='bar', stacked = True, title=str(n_clust), legend=False)
 
-cl_count
-dat_toPlot
+        score_gini = sum(gini_index(cl_count))
+        cluster_scores_gini.append(score_gini)
 
+        score_ent = sum(entropy(cl_count))
+        cluster_scores_ent.append(score_ent)
+
+        score = sum(measure_clusters(cl_count))
+        cluster_scores.append(score)
+    pl.plot(cluster_scores)
+    pl.plot(cluster_scores_gini)
+    pl.plot(cluster_scores_ent)
+
+main()
+
+# below here is all testing, no need to actually run it.
 
 def test_measure():
-    mock_clusters = list()
+    mock_clusters = pd.DataFrame(index = range(50*12), columns = ['i', 'Cluster'])
     for i in range(50):
-        mock_cluster = list()
-        rand1 = random.randint(0, 12)
-        mock_clusters.append(rand1)
-        current_sum = rand1
-        while current_sum<12:
-            num_left = 12-current_sum
-            new_rand = random.randint(0, num_left)
-            mock_clusters.append(new_rand)
-            current_sum = current_sum + new_rand
-            # print(current_sum)
-        # print(mock_cluster)
-        mock_clusters.append(mock_cluster)
-mock_clusters
+        for j in range(12):
+            mock_clusters.iloc[(12*i)+j]['i'] = str(i)
+            mock_clusters.iloc[(12*i)+j]['Cluster'] = random.randint(0,8)
+    mock_clusters
+
+
+    mock_clust_count = count_clusters(mock_clusters, grouper1='i', counter='Cluster')
+    gini_scores = gini_index(mock_clust_count)
+    ent_scores = entropy(mock_clust_count)
+    gini_scores
+    ent_scores
+
+    ax = mock_clust_count.transpose().plot(kind='bar', stacked = True, legend=False)
+    ax.set_xticklabels(scores_str)
+    fig = ax.get_figure()
+    fig.savefig('test.png')
+    mock_clust_count
