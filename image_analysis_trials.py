ee63dbbd3ee0e04b23cdc3e5b7c9040f6ce3eb03
Emily Gould <e.m.gould@sms.ed.ac.uk>
2020-06-24 15:52:22 +0100

    Added cells to tidy up and looked more at labelling the regions
    used https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_label.html#sphx-glr-auto-examples-segmentation-plot-label-py, need to figure out why the plotting doesn't work like it did in the example

--- image_analysis_trials.py
+++ image_analysis_trials.py
@@ -1,4 +1,5 @@
-from skimage import filters, io, draw, transform, measure, segmentation, color
+# %% load packages
+from skimage import filters, io, draw, transform, measure, segmentation, color, morphology
 from skimage.future import graph
 import numpy as np
 from sklearn import preprocessing
@@ -6,6 +7,7 @@ import matplotlib.pyplot as pl
 import math
 import pandas as pd
 
+# %% read in file
 imFile = '/Users/s1101153/Desktop/TIFs/SUM_2014_5_30-T3M_7_63xoil_1.tif'
 
 imArr = io.imread(imFile)
@@ -13,6 +15,7 @@ imArr.shape
 
 io.imshow(imArr[:,:,2])
 
+# %% create mask for segment slice, using coords for centre of droplet
 L = 2048
 um = 72.58
 pixel_um=L/um
@@ -40,6 +43,8 @@ xx, yy = draw.polygon([C_x, x1, x2, C_x], [C_y, y1, y2, C_y], shape = mask_poly.
 mask_poly[xx,yy] = 1
 
 mask = mask_circle & mask_poly
+
+
 #
 # pl.imshow(mask)
 #
@@ -61,6 +66,7 @@ mask = mask_circle & mask_poly
 #
 # pl.imshow(slice2_masked)
 
+# %% use mask to slice image into 12 slices
 green_slices = list()
 red_slices = list()
 
@@ -85,18 +91,28 @@ for i in range(n_slices):
     im = transform.rotate(im, np.degrees(theta1), center = [C_y, C_x])
 
 
-
-# threshold image
+# trying different methods of thresholding the image
+# %% global thresholding using otsu
 image = imArr.copy()[:,:,2]
 threshold_val = filters.threshold_otsu(image)
 
 labeled_foreground = (image>threshold_val)#.astype(int)
 io.imshow(labeled_foreground)
+pl.show()
 io.imshow(image)
+pl.show()
 area_frac = np.sum(labeled_foreground)
+print(area_frac)
+
+thr_global2 = filters.threshold_otsu(image2)
+binary_global2 = image2>thr_global2
+io.imshow(binary_global2)
+pl.show()
+io.imshow(image2)
+pl.show()
 
 
-
+# %% local thresholding, trying different definitions of 'local'
 for i in range(20):
     block_size = i*10 +1
     thr_local = filters.threshold_local(image, block_size, offset=10)
@@ -117,11 +133,7 @@ for i in range(20):
     pl.show()
 
 
-thr_global2 = filters.threshold_otsu(image2)
-binary_global2 = image2>thr_global2
-pl.imshow(binary_global2)
-
-# image segmentation
+# %% trying image segmentation
 labels1 = segmentation.slic(image.astype('double'), compactness=30, n_segments=400)
 out1 = color.label2rgb(labels1, image, kind='avg')
 
@@ -141,7 +153,7 @@ pl.tight_layout()
 pl.show()
 
 
-
+# %% high-pass filtering (and random little plotting function)
 def plot(data, title):
     plot.i += 1
     pl.subplot(3,2,plot.i)
@@ -149,7 +161,6 @@ def plot(data, title):
     # pl.gray()
     pl.title(title)
 
-# high-pass filtering
 plot.i = 0
 plot(image, 'Original1')
 plot(image, 'Original2')
@@ -162,39 +173,107 @@ hp2 = image2-g2
 plot(hp1, 'HighPass1')
 plot(hp2, 'HighPass2')
 pl.show()
-# pl.savefig('highpassFilters.png')
 
+# %% otsu thresholding on high-pass filtered images
 thr_hp1 = filters.threshold_otsu(hp1)
 bin_hp1 = hp1>thr_hp1
 io.imshow(bin_hp1)
+pl.show()
 
 thr_hp2 = filters.threshold_otsu(hp2)
 bin_hp2 = hp2>thr_hp2
 io.imshow(bin_hp2)
+pl.show()
 
-
-# region properties
+# %% calculating circularity of shape from otsu thresholded image using region properties
+# will need to think more about this, circularity of the droplet won't make sense in the slices
 label_img = measure.label(image)
 io.imshow(label_img)
 
 label_otsu_image = measure.label(labeled_foreground)
-np.unique(label_otsu_image)
+print(np.unique(label_otsu_image))
 io.imshow(label_otsu_image)
+pl.show()
 io.imshow(labeled_foreground)
+pl.show()
 
 label_hp_thr_image = measure.label(bin_hp1)
 io.imshow(label_hp_thr_image)
-
+pl.show()
 
 
 label_props = measure.regionprops_table(label_otsu_image, properties=('filled_area','perimeter','centroid'))
 
 otsu_region_props = pd.DataFrame(label_props)
-otsu_region_props
+# print(otsu_region_props)
 # biggest_region = otsu_region_props[otsu_region_props['filled_area']==max(otsu_region_props['filled_area'])]
 # circularity = 4*math.pi*biggest_region['filled_area']/biggest_region['perimeter']**2
 otsu_region_props['circularity'] = 4*math.pi*otsu_region_props['filled_area']/otsu_region_props['perimeter']**2
-otsu_region_props
+print(otsu_region_props)
 
 raw_props = measure.regionprops_table(image.astype(int), properties=('filled_area', 'perimeter', 'centroid'))
 pd.DataFrame(raw_props)
+
+# %% understanding region labelling better
+
+io.imshow(image)
+pl.show()
+thresh=filters.threshold_otsu(image)
+plain_thresh_im = image>thresh
+# io.imshow(plain_thresh_im)
+# pl.show()
+bw = morphology.closing(image>thresh, morphology.square(2))
+io.imshow(bw)
+pl.show()
+
+cleared = segmentation.clear_border(bw)
+io.imshow(cleared)
+pl.show
+
+label_image = measure.label(cleared)
+image_label_overlay = color.label2rgb(label_image, image=bw, bg_label=0)
+io.imshow(image_label_overlay)
+
+fig, ax = pl.subplots(figsize=(10,6))
+ax.imshow(image_label_overlay)
+
+import matplotlib.patches as mpatches
+for region in measure.regionprops(label_image):
+    if region.area >= 0:
+        minr, minc, maxr, maxc = region.bbox
+        rect = mpatches.Rectangle((minc, minr), maxc-minc, maxr-minr, fill=False, edgecolor='red', linewidth=2)
+        ax.add_patch(rect)
+ax.set_axis_off()
+pl.tight_layout()
+pl.show()
+
+
+
+
+# %% function to apply the above to any image
+def threshold_and_label(img, regionsize):
+    thresh=filters.threshold_otsu(img)
+    plain_thresh_im = img>thresh
+    bw = morphology.closing(img>thresh, morphology.square(2))
+
+    cleared = segmentation.clear_border(bw)
+
+
+    label_image = measure.label(cleared)
+    image_label_overlay = color.label2rgb(label_image, image=bw, bg_label=0)
+
+    fig, ax = pl.subplots(figsize=(10,6))
+    ax.imshow(image_label_overlay)
+
+    import matplotlib.patches as mpatches
+    for region in measure.regionprops(label_image):
+        if region.area >= regionsize:
+            minr, minc, maxr, maxc = region.bbox
+            rect = mpatches.Rectangle((minc, minr), maxc-minc, maxr-minr, fill=False, edgecolor='red', linewidth=2)
+            ax.add_patch(rect)
+    ax.set_axis_off()
+    pl.tight_layout()
+    pl.show()
+
+threshold_and_label(image, 0)
+threshold_and_label(image2, 100)
