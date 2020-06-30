015dec6088b836e394c52ed87eb6782fcf04dad1
Emily Gould <e.m.gould@sms.ed.ac.uk>
2020-06-30 16:55:38 +0100

    cluster_plot fixed after changing count_clusters function in data_tools

--- cluster_plot.py
+++ cluster_plot.py
@@ -5,6 +5,6 @@ from data_tools import *
 dat = pd.read_csv('/Users/s1101153/Dropbox/Emily/Results/2020-05-26_ACF_clusters_3pt-av_small.csv', index_col=0)
 # dat = dat.drop(['ACF_index', 'Value'], axis='columns').drop_duplicates().reset_index(drop=True)
 
-cluster_count = count_clusters(dat, 'Sample', 'Colour', counter='Cluster')
+cluster_count = count_clusters(dat, counter='Cluster', grouper1='Sample', grouper2='Colour')
 
 cluster_count.transpose().plot(kind='bar', stacked = True)
